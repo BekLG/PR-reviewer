70d7c18058763dbdf623dfc39750a6e282a27e0e
bek <54703839+BekLG@users.noreply.github.com>
2025-09-24 12:43:13 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+def divide_numbers(a, b):
+    return a / b  
+
+def get_user_age():
+    age = input("Enter your age: ") 
+    if age < 18:  
+        print("You are a minor.")
+    else:
+        print("You are an adult.")
