080b066e1840eaa450db4e615d16c52c3dd91ec8
bek <54703839+BekLG@users.noreply.github.com>
2025-10-01 12:21:31 +0300

    Update check.py

--- scripts/check.py
+++ scripts/check.py
@@ -4,12 +4,6 @@ for line in diff_output.splitlines():
         files[current_file] = []
         position = 0
     elif line.startswith("@@"):
-        # Example: @@ -12,0 +13,2 @@
-        hunk_header = line
-        parts = line.split()
-        added_range = parts[2]  # e.g., +13,2
-        start_line = int(added_range.split(",")[0][1:])
-        position = start_line
     elif current_file and line.startswith("+") and not line.startswith("+++"):
         files[current_file].append({"line": position, "code": line[1:]})
         position += 1
